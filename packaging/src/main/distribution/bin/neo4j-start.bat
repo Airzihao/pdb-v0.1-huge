f64380a6e354191552d36fcaf9df5c0c79ee23f9
Airzihao <airzihao@163.com>
2020-09-29 17:01:04 +0800

    ·

---
+++ packaging/src/main/distribution/bin/neo4j-start.bat
@@ -0,0 +1,31 @@
+@echo off
+set batchfile=%0
+set full=%~f0
+setlocal
+  for %%d in (%~dp0.) do set Directory=%%~fd
+  set Directory=%Directory%
+  for %%d in (%~dp0..) do set ParentDirectory=%%~fd
+  set ParentDirectory=%ParentDirectory%
+  set class_path=%ParentDirectory%\lib\pandadb-dist-0.1.0-SNAPSHOT.jar
+  set MainClass="cn.pandadb.server.PandaServerStarter"
+  set config=%ParentDirectory%\conf\neo4j.conf
+  set log_path=%ParentDirectory%\logs\neo4j.log
+
+echo Starting Pandadb...
+
+for /f "tokens=1,2 delims==" %%a in (%config%) do (
+  if "%%a"=="dbms.connector.bolt.listen_address" set bolt=%%b
+)
+echo Bolt enabled on %bolt%
+echo Started
+
+for /f "tokens=1,2 delims==" %%a in (%config%) do (
+  if "%%a"=="dbms.connector.http.listen_address" set http=%%b
+)
+echo Remote interface available at %http%
+echo There may be a short delay until the server is ready.
+
+start java -jar %class_path% %ParentDirectory% %config%
+endlocal
+Pause
+exit
